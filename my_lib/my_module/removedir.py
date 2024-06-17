2843a86dc701a2118f4ad7c6cf9c773576ce55ba
Jaco van Gelder <jaco.van.gelder@inter.ikea.com>
2024-06-17 17:12:17 +0200

    function paranthesis

--- my_lib/my_module/removedir.py
+++ my_lib/my_module/removedir.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession, DataFrame
 from my_module.utils import get_dbutils
 
 def remove_dir_using_dbutils():
-    get_dbutils.fs.rm('directory/to/remove', True)
+    get_dbutils().fs.rm('directory/to/remove', True)
 
 def read_spark_table(table) -> DataFrame:
     spark = SparkSession.getActiveSession()
