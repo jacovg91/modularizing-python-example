548c9be676432bb2ac03c7da23ecf13fceb3dc6a
Jaco van Gelder <jaco.van.gelder@inter.ikea.com>
2024-06-17 17:14:20 +0200

    fixes

--- my_lib/my_module/removedir.py
+++ my_lib/my_module/removedir.py
@@ -2,8 +2,8 @@ from pyspark.sql import SparkSession, DataFrame
 
 from my_module.utils import get_dbutils
 
-def remove_dir_using_dbutils():
-    get_dbutils().fs.rm('directory/to/remove', True)
+def remove_dir_using_dbutils(directory_to_remove):
+    get_dbutils().fs.rm(directory_to_remove, True)
 
 def read_spark_table(table) -> DataFrame:
     spark = SparkSession.getActiveSession()
