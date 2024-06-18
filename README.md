cbce7c5d1ecef667c4a1461d80ee09b8e12bcd64
Jaco van Gelder <jaco.van.gelder@inter.ikea.com>
2024-06-18 19:16:28 +0200

    nits

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Steps to follow:
 import sys
 sys.path.append("my_lib")
 ```
-- Then import functions the following way
+- Then import functions the following way:
 ```
 from <my_lib>.<my_module> import <function_name>, <ClassName>
 ```
--- my_lib/my_module/module_name.py
+++ my_lib/my_module/module_name.py
@@ -2,8 +2,10 @@ from pyspark.sql import SparkSession, DataFrame
 
 from my_module.utils import get_dbutils
 
+
 def remove_dir_using_dbutils(directory_to_remove):
     get_dbutils().fs.rm(directory_to_remove, True)
+
 
 def read_spark_table(table) -> DataFrame:
     spark = SparkSession.getActiveSession()
